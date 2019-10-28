4a300797e37ea7f4712ef2d35b00682a08ab0631
Alexi Chepura <a.chepura@gmail.com>
2019-10-28 10:25:03 +0300

    reorder props deps

--- example/browser.tsx
+++ example/browser.tsx
@@ -4,8 +4,8 @@ import { DbClient } from "./db"
 
 new Chyk<TDeps>({
   routes,
+  deps: { apiSdk: new DbClient() },
   data: (window as any).ssr_data,
   statusCode: (window as any).ssr_statusCode,
   el: document.getElementById("app"),
-  deps: { apiSdk: new DbClient() },
 })
