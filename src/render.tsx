372faa411d163b152233d899c6ff400bcb9876a3
Alexi Chepura <a.chepura@gmail.com>
2019-11-09 19:17:56 +0300

    move WrapperComponent to ChykComponent

--- src/render.tsx
+++ src/render.tsx
@@ -11,28 +11,21 @@ export const chykHydrateOrRender = (chyk: Chyk) => {
     throw "No renderer for no element"
   }
   const renderer = chyk.el.childNodes.length === 0 ? render : hydrate
-  const WrapperComponent = chyk.component
-  if (WrapperComponent) {
-    renderer(
-      <WrapperComponent>
-        <ChykComponent chyk={chyk} />
-      </WrapperComponent>,
-      chyk.el
-    )
-  } else {
-    renderer(<ChykComponent chyk={chyk} />, chyk.el)
-  }
+  renderer(<ChykComponent chyk={chyk} />, chyk.el)
 }
 
 export const ChykComponent: FC<{ chyk: Chyk }> = ({ chyk }) => {
   if (!chyk.history) {
     throw "No history"
   }
+  const WrapperComponent = chyk.component || React.Fragment
   return (
     <ChykContext.Provider value={chyk}>
       <Router history={chyk.history}>
         <ChykPreloader>
-          <DataRoutes routes={chyk.routes} />
+          <WrapperComponent>
+            <DataRoutes routes={chyk.routes} />
+          </WrapperComponent>
         </ChykPreloader>
       </Router>
     </ChykContext.Provider>
@@ -41,13 +34,16 @@ export const ChykComponent: FC<{ chyk: Chyk }> = ({ chyk }) => {
 ChykComponent.displayName = "ChykComponent"
 
 export const ChykStaticComponent: FC<{ chyk: Chyk }> = ({ chyk }) => {
+  const WrapperComponent = chyk.component || React.Fragment
   return (
     <ChykContext.Provider value={chyk}>
       <StaticRouter
         location={chyk.currentLocationState.location}
         context={chyk.staticRouterContext}
       >
-        <DataRoutes routes={chyk.routes} />
+        <WrapperComponent>
+          <DataRoutes routes={chyk.routes} />
+        </WrapperComponent>
       </StaticRouter>
     </ChykContext.Provider>
   )
