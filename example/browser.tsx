3ebeb9b9cf8bae7f2649e6ef02eee4544fbe6759
Alexi Chepura <a.chepura@gmail.com>
2019-11-19 23:11:20 +0300

    interface Window

--- example/browser.tsx
+++ example/browser.tsx
@@ -2,10 +2,17 @@ import { Chyk } from "../src/chyk"
 import { routes, TDeps } from "./app"
 import { DbClient } from "./db"
 
+declare global {
+  interface Window {
+    ssr_data: any
+    ssr_statusCode: number
+  }
+}
+
 new Chyk<TDeps>({
   routes,
   deps: { apiSdk: new DbClient() },
-  data: (window as any).ssr_data,
-  statusCode: (window as any).ssr_statusCode,
+  data: window.ssr_data,
+  statusCode: window.ssr_statusCode,
   el: document.getElementById("app"),
 })
